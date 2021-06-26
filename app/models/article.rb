e42c3b4833040c417ff1fde636c4efa3f1b9187f
initial commit <>
2021-06-26 10:39:37 +0900

    flashメッセージの表示と日本語化

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+    # length 長さをチェック
+    # format 正規表現と一致するかチェック
+    # numericality 数字のチェック
+    # presence 入力されているかチェック
+    # uniqueness 一意であるかチェック
 end
