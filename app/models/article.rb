4dc36502e9201e91fb1bcdf2f1ab4c10bfe22129
initial commit <>
2021-06-27 13:13:40 +0900

    自分の記事に対してアクションボタンを表示するように変更

--- app/models/article.rb
+++ app/models/article.rb
@@ -28,7 +28,7 @@ class Article < ApplicationRecord
 
     validate :validate_title_and_content_length
 
-    belongs_to :user, dependent: :destroy
+    belongs_to :user
 
     # numericality 数字のチェック
     def display_created_at
