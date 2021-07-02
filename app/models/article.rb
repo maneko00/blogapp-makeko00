c919f7f012b5441509ab9984bc291ee0f912803b
initial commit <>
2021-07-03 06:27:49 +0900

    ActiveDecoratorの導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -30,15 +30,4 @@ class Article < ApplicationRecord
 	belongs_to :user
 
 	# numericality 数字のチェック
-	def display_created_at
-		I18n.l(self.created_at, format: :default)
-	end
-
-    def author_name
-		user.display_name
-    end
-
-    def like_count
-		likes.count
-    end
 end
