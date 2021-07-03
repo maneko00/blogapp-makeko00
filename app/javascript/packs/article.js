bb340eca11d142076c3347faca28eedb842bb86a
initial commit <>
2021-07-03 21:27:04 +0900

    リファクタリング

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -12,6 +12,20 @@ const handleHeartDisplay = (hasLiked) => {
     }
 }
 
+const handleCommentForm = () => {
+    $('.show-comment-form').on('click', () => {
+      $('.show-comment-form').addClass('hidden')
+      $('.comment-text-area').removeClass('hidden')
+    })
+}
+  
+const appendNewComment = (comment) => {
+    $('.comments-container').append(
+        `<div class="article_comment"><p>${comment.content}</p></div>`
+    )
+}
+  
+
 document.addEventListener('DOMContentLoaded', () => {
     const dataset = $('#article-show').data()
     const articleId = dataset.articleId
@@ -20,17 +34,14 @@ document.addEventListener('DOMContentLoaded', () => {
         .then((response) => {
             const comments = response.data
             comments.forEach((comment) => {
-                $('.comments-container').append(
-                  `<div class="article_comment"><p>${comment.content}</p></div>`
-                )
+                appendNewComment(comment)
             })
         })
+        .catch((error) => {
+            window.alert('失敗!')
+        })
 
-
-    $('.show-comment-form').on('click', () => {
-        $('.show-comment-form').addClass('hidden')
-        $('.comment-text-area').removeClass('hidden')
-    })
+    handleCommentForm()
 
     $('.add-comment-button').on('click', () => {
         const content = $('#comment_content').val()
@@ -42,9 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
             })
                 .then((res) => {
                     const comment = res.data
-                    $('.comments-container').append(
-                        `<div class="article_comment"><p>${comment.content}</p></div>`
-                    )
+                    appendNewComment(comment)
                     $('#comment_content').val('')
                 })
         }
